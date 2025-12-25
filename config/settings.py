3399f99748cecd28e58c8b72d99119d0f7b0e90f
Muhammadjon Ibrohimov <ibrokhimovmiu@gmail.com>
2025-12-25 20:15:27 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -82,24 +82,24 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "e_commerce",
-        "USER": "postgres",
-        "PASSWORD": "197346825Miu",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "e_commerce",
+#         "USER": "postgres",
+#         "PASSWORD": "197346825Miu",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
+#     }
+# }
 
 
 # Password validation
